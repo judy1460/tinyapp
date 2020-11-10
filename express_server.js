5b2494cf8b5231c468af703877225b6801506c0c
Mina <minatoosa@yahoo.com>
2020-11-10 00:15:56 +0000

    modify main

--- express_server.js
+++ express_server.js
@@ -22,3 +22,12 @@ app.listen(PORT, () => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
