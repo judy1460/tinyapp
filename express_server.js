faa1245b6e906804f3693d833f44fbbde3f3c410
Mina <minatoosa@yahoo.com>
2020-11-10 06:15:43 +0000

    add new server

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -39,9 +40,20 @@ app.get("/set", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  console.log(templateVars);
   res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString () {
+  let rand = Math.random().toString(36).substring(6);
+  return rand;
+};
