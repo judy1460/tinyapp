8da913d8759381c60e157c070f6388b45ed73b4d
Mina <minatoosa@yahoo.com>
2020-11-13 00:48:55 +0000

    update tiny

--- express_server.js
+++ express_server.js
@@ -3,21 +3,30 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+const { json } = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser())
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+const urlsForUser = function(id) {
+  let newUrlDatabase = {};
+  for (let item in urlDatabase) {
+    if (urlDatabase[item].userId === id) {
+      newUrlDatabase[item] =  urlDatabase[item];
+    }
+}
+return newUrlDatabase;
+}
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies["userId"]]};
+  const templateVars = { urls: urlsForUser(req.cookies["userId"]), user: users[req.cookies["userId"]]};
   res.render("urls_index", templateVars);
 });
 
@@ -43,11 +52,16 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  if(req.cookies["userId"]){
+    res.render("urls_new");
+  }else {
+    res.redirect("/login");
+  }
+  
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], users: user[req.cookies["userId"]]}
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["userId"]]}
   res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
@@ -61,14 +75,29 @@ app.post("/urls", (req, res) => {
 };*/
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+  const obj = urlDatabase[shortURL];
+  res.redirect(obj.longURL);
 });
 app.post("/urls/:url/delete", (req, res)=>{
+  if(req.cookies["userId"] !== undefined && req.cookies["userId"] !== null ){
   const url = req.params.url;
   delete urlDatabase[url];
   res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
+
+/*app.post("/urls/:url/edit", (req, res) =>{
+  if(req.cookies["userId"] !== undefined && req.cookies["userId"] !== null ){
+    const url = req.params.url;
+    edit urlDatabase[url];
+    res.redirect("/urls");
+    } else {
+      res.redirect("/login");
+    }
+
+})*/
 
 app.post("/urls/:url", (req, res)=>{
   const url = req.params.url;
@@ -77,11 +106,14 @@ app.post("/urls/:url", (req, res)=>{
 });
 
 app.get('/login', (req, res)=>{
-  res.render('login');
+ 
+  const templateVars = {user:users[req.cookies["userId"]]}
+  res.render('login', templateVars);
 });
 
 app.get('/register', (req, res)=>{
-  res.render('register');
+  const templateVars = {user:users[req.cookies["userId"]]}
+  res.render('register', templateVars);
 });
 
 app.post("/register",(req, res) =>{
@@ -106,10 +138,15 @@ app.post("/login", (req, res)=>{
   if(!email || !password){
     return res.status(403).send("Please provide right email or password")
   }
-  for (const id in users){
-    if(email === users[id].email){
-      return res.status(403).send("Email is existed.");
+  for (const id in users) {
+    if(email === users[id].email) {
+      if(password === users[id].password) {
+         req.cookies["userId"];
+         res.redirect("/urls");
+    } else {
+      return res.status(403).send("Error");
     }
+    };
   };
   const id = generateRandomString();
   const user = {id, email, password};
@@ -122,6 +159,7 @@ app.post('/logout', (req, res)=>{
   res.clearCookie("userId")
   res.redirect("/urls")
 });
+
 const generateRandomString = ()=> Math.random().toString(36).substring(2, 8);
 const users = { 
   "001": {
